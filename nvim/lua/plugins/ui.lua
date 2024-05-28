693653de8c13b3648c4006b61e176aee100785ce
Lior Frenkel <lior.frenkel@melio.com>
2024-05-28 09:35:34 +0300

    Fix ui plugins

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "echasnovski/mini.indentscope",
-    opts = {
-      draw = {
-        delay = 10,
-        animation = require("mini.indentscope").gen_animation.none(),
-      },
-    },
-  },
-  {
     "rcarriga/nvim-notify",
     opts = {
       timeout = 4000,
