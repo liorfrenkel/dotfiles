3f84742486804e2a3f6064d536884269afcb0232
Lior Frenkel <lior.frenkel@melio.com>
2024-09-05 20:31:38 +0300

    Hide problematic error from vtsls

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,16 +1,28 @@
 return {
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 4000,
-    },
-  },
-  {
-    "folke/noice.nvim",
-    opts = {
-      messages = {
-        enabled = true,
-      },
-    },
-  },
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 3000,
+		},
+		init = function()
+			local notify = require("notify")
+			local filtered_notify = function(...)
+				local msg, level = select(1, ...)
+				if level == vim.log.levels.ERROR and string.match(msg, "vtsls: %-%d+") then
+					return -- ignored
+				end
+				return notify(...)
+			end
+			vim.notify = filtered_notify
+		end,
+	},
+	{
+		"folke/noice.nvim",
+    enabled = false,
+		opts = {
+			messages = {
+				enabled = true,
+			},
+		},
+	},
 }
