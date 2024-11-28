fa0cf80e51fe64b742706efc22816d95e26d215b
Lior Frenkel <lior.frenkel@melio.com>
2024-11-28 13:36:28 +0200

    Add ToggleBoolean and ability to toggle only on describe

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,19 +12,29 @@ vim.keymap.set("n", "<leader>ctr", "<cmd>TSToolsRenameFile<cr>", { desc = "types
 vim.keymap.set("n", "<leader>cto", "<cmd>TSToolsOrganizeImports<cr>", { desc = "typescript organize imports" })
 vim.keymap.set("n", "<leader>cta", "<cmd>TSToolsAddMissingImports<cr>", { desc = "typescript add missing imports" })
 
--- Function to toggle '.only'
 function ToggleTestOnly()
 	local line = vim.api.nvim_get_current_line() -- Get current line
 	if line:match("%.only") then
 		-- If the line contains '.only', remove it
 		line = line:gsub("%.only", "")
 	else
-		-- Otherwise, insert '.only' after 'it' or 'test'
+		-- Otherwise, insert '.only' after 'it' or 'test' or 'describe'
 		line = line:gsub("^(%s*it)", "%1.only")
 		line = line:gsub("^(%s*test)", "%1.only")
+		line = line:gsub("^(%s*describe)", "%1.only")
 	end
 	vim.api.nvim_set_current_line(line) -- Set the modified line
 end
 
--- Map the function to a key, e.g., <leader>o
-vim.keymap.set("n", "<leader>to", ":lua ToggleTestOnly()<CR>", { desc = "toggle .only in test"})
+function ToggleBoolean()
+	local line = vim.api.nvim_get_current_line() -- Get current line
+	if line:match("false") then
+		line = line:gsub("false", "true", 1)
+	elseif line:match("true") then
+		line = line:gsub("true", "false", 1)
+	end
+	vim.api.nvim_set_current_line(line) -- Set the modified line
+end
+
+vim.keymap.set("n", "<leader>to", ":lua ToggleTestOnly()<CR>", { desc = "toggle .only in test" })
+vim.keymap.set("n", "<leader>tb", ":lua ToggleBoolean()<CR>", { desc = "toggle next boolean" })
