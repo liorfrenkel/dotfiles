df862e72c52233f183b07ed928039e596a1306f8
Lior Frenkel <lior.frenkel@melio.com>
2025-05-12 21:47:19 +0300

    Delete lazy files

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.vscode then
-  require("vscode-config")
-else
-  -- bootstrap lazy.nvim, LazyVim and your plugins
-  require("config.lazy")
-  require("config.commands")
-end
--- nvim/lua/vscode-config/init.lua
+++
@@ -1,2 +0,0 @@
-require('vscode-config.options')
-require('vscode-config.remaps')
