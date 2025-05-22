be272be5f9b4d4f9733119d9fa1e951777b92e1b
Lior Frenkel <lior.frenkel@melio.com>
2025-05-22 14:03:58 +0300

    Add neotree keys

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -21,7 +21,84 @@ return {
     lazy = false, -- neo-tree will lazily load itself
     ---@module "neo-tree"
     opts = {
-      -- fill any relevant options here
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = false },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        position = 'left',
+        width = 40,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+        mappings = {
+          ['<2-LeftMouse>'] = 'open',
+          ['<cr>'] = 'open',
+          ['<esc>'] = 'cancel', -- close preview or floating neo-tree window
+          ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
+          -- Read `# Preview Mode` for more information
+          ['S'] = 'open_split',
+          ['s'] = 'open_vsplit',
+          ['t'] = 'open_tabnew',
+          ['C'] = 'close_node',
+          -- ['C'] = 'close_all_subnodes',
+          ['z'] = 'close_all_nodes',
+          --["Z"] = "expand_all_nodes",
+          --["Z"] = "expand_all_subnodes",
+          ['a'] = {
+            'add',
+            -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+            -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+            config = {
+              show_path = 'none', -- "none", "relative", "absolute"
+            },
+          },
+          ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+          ['d'] = 'delete',
+          ['r'] = 'rename',
+          ['b'] = 'rename_basename',
+          ['y'] = 'copy_to_clipboard',
+          ['x'] = 'cut_to_clipboard',
+          ['p'] = 'paste_from_clipboard',
+          ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
+          -- ["c"] = {
+          --  "copy",
+          --  config = {
+          --    show_path = "none" -- "none", "relative", "absolute"
+          --  }
+          --}
+          ['m'] = 'move', -- takes text input for destination, also accepts the optional config.show_path option like "add".
+          ['q'] = 'close_window',
+          ['R'] = 'refresh',
+          ['?'] = 'show_help',
+          ['<'] = 'prev_source',
+          ['>'] = 'next_source',
+          ['i'] = 'show_file_details',
+          -- ["i"] = {
+          --   "show_file_details",
+          --   -- format strings of the timestamps shown for date created and last modified (see `:h os.date()`)
+          --   -- both options accept a string or a function that takes in the date in seconds and returns a string to display
+          --   -- config = {
+          --   --   created_format = "%Y-%m-%d %I:%M %p",
+          --   --   modified_format = "relative", -- equivalent to the line below
+          --   --   modified_format = function(seconds) return require('neo-tree.utils').relative_date(seconds) end
+          --   -- }
+          -- },
+
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['Y'] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg('+', path, 'c')
+            end,
+            desc = 'Copy Path to Clipboard',
+          },
+        },
+      },
     },
     keys = {
       { '<leader>e', '<cmd>Neotree toggle reveal<cr>', desc = 'Toggle neotree' },
