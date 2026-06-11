5018c69b43e69883f6bb1f89fc98423dcc4b51fc
Lior Frenkel <lior.frenkel@melio.com>
2026-06-11 12:24:29 +0300

    Add more keybinds to diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -7,5 +7,28 @@ return {
     { '<leader>gdo', '<cmd>DiffviewOpen<cr>', desc = 'Open Diffview' },
     { '<leader>gdm', '<cmd>DiffviewOpen main<cr>', desc = 'Open Diffview vs main' },
     { '<leader>gdc', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
+    { '<leader>gdh', '<cmd>DiffviewFileHistory<cr>', desc = 'Browse commit history' },
+    {
+      '<leader>gdi',
+      function()
+        vim.ui.input({ prompt = 'Diff HEAD vs commit: ' }, function(ref)
+          if ref and ref ~= '' then
+            vim.cmd('DiffviewOpen ' .. ref)
+          end
+        end)
+      end,
+      desc = 'Diff HEAD vs commit (prompt)',
+    },
+    {
+      '<leader>gdp',
+      function()
+        vim.ui.input({ prompt = 'Diff single commit vs its parent: ' }, function(ref)
+          if ref and ref ~= '' then
+            vim.cmd('DiffviewOpen ' .. ref .. '^..' .. ref)
+          end
+        end)
+      end,
+      desc = 'Diff single commit vs its parent (prompt)',
+    },
   },
 }
