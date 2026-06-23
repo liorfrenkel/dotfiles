559e12f5fdc3c5c4d761d85a523b89730eebe5b9
Lior Frenkel <lior.frenkel@melio.com>
2026-06-23 16:21:53 +0300

    Fix diffview vs main

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -5,7 +5,7 @@ return {
   },
   keys = {
     { '<leader>gdo', '<cmd>DiffviewOpen<cr>', desc = 'Open Diffview' },
-    { '<leader>gdm', '<cmd>DiffviewOpen main<cr>', desc = 'Open Diffview vs main' },
+    { '<leader>gdm', '<cmd>DiffviewOpen main...HEAD<cr>', desc = 'Open Diffview vs main' },
     { '<leader>gdc', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
     { '<leader>gdh', '<cmd>DiffviewFileHistory<cr>', desc = 'Browse commit history' },
     {
